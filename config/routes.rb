1bac6b63a6a9f1fd5178e0ba749dc0e6cec07aef
jenchan-938 <jchan5@uchicago.edu>
2024-11-05 22:13:20 +0000

    grade_round1

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
 
   get("/directors", { :controller => "directors", :action =>"index"})
-  
+
+  get("/directors/:", 
 
 
 end
