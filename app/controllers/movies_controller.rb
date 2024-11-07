fca1c050f73313918ca48df53869fd0577d57e83
jenchan-938 <jchan5@uchicago.edu>
2024-11-07 05:01:46 +0000

    1101

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,6 @@
 class MoviesController < ApplicationController
   def index
-    render({ :template => "movie_template/list"})
+    render({ :template => "movie_templates/list"})
   end
 
 def show
@@ -8,7 +8,7 @@ def show
   matching_records =Movie.where({ :id=> the_id})
   @the_movie =matching_records.at(0)
 
-  render ({ :template => "movie_template/details"})
+  render ({ :template => "movie_templates/details"})
 end
 
 end
